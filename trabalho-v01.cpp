065b09361dc778b225e332ae76cdff091f5ac65b
edersonlrf <edersonluis.rf@gmail.com>
2018-04-10 19:55:01 -0300

    update

--- trabalho-v01.cpp
+++ trabalho-v01.cpp
@@ -14,9 +14,10 @@ typedef struct aluno {
 
 void inserir(turma *aluno, char nome[50], float media, int ano);
 void listarEmOrdem(turma aluno);
-int remover(turma *aluno, char nome[50]);
+int remover(turma *aluno, char nome[50], int *acho);
 turma maior(turma *aluno);
 void parentesesAninhados(turma aluno);
+void removerQuebraLinha(char *nome);
 
 main() {
 	setlocale(LC_ALL, "Portuguese");
@@ -27,7 +28,18 @@ main() {
 	float media;
 	int ano;
 	
-	int op = 0;
+	int op = 0, len = 0, achou = 0;
+	
+	/*inserir(&t, "maria", 	5, 2010);
+	inserir(&t, "jose", 	6, 2010);
+	inserir(&t, "marcelo",	7, 2010);
+	inserir(&t, "artur" ,	8, 2010);
+	inserir(&t, "ana", 		9, 2010);
+	inserir(&t, "cezar", 	10, 2010);
+	inserir(&t, "claudio", 	11, 2010);
+	inserir(&t, "raul", 	12, 2010);
+	inserir(&t, "zulmira", 	13, 2010);
+	inserir(&t, "vander", 	14, 2010);*/
 
 	while (op != 5) {
 		system("cls");
@@ -37,7 +49,7 @@ main() {
 		gotoxy(2,6); printf("2 - Listar Em-ordem.");
 		gotoxy(2,8); printf("3 - Remover Aluno.");
 		gotoxy(2,10); printf("4 - Parênteses Aninhados.");
-		gotoxy(2,12); printf("5 - Sair");
+		gotoxy(2,12); printf("5 - Sair.");
 		
 		gotoxy(2,16); printf("Escolha uma Opção: ");
 		scanf("%d", &op);
@@ -46,45 +58,67 @@ main() {
 		system("cls");
 		
 		if (op == 1) {
-			gotoxy(2,2); printf("Nome......: ");
+			gotoxy(2,2); printf("Cadastro de Aluno: ");
+			
+			gotoxy(2,4); printf("Nome Completo.........: ");
 			fflush(stdin);
 			fgets(nome,50,stdin);
+			removerQuebraLinha(nome);
 
-			gotoxy(2,4); printf("Media.....: ");
+			gotoxy(2,6); printf("Media Final (10,0)....: ");
 			scanf("%f",&media);
 
-			gotoxy(2,6); printf("Ano.......: ");
+			gotoxy(2,8); printf("Ano Que Cursou........: ");
 			scanf("%i",&ano);
 			
+			gotoxy(2,10); printf("Caminho da inserção: \n\n");
+
 			inserir(&t, nome, media, ano);
+			
+			printf("\n\nPressione algo para continuar....");
 			
 			getch();
 		}
 		
 		if (op == 2) {
 			if (t == NULL) {
-				printf("Arvore Vazia!");
+				printf("\n\n\n\n\t\tNenhum aluno cadastrado!\n\n\n\n\n\t Pressione uma tecla para continuar...");
 			} else {
+				printf("Lista de Alunos:\n\n");
 				listarEmOrdem(t);
+				printf("\n\nPressione algo para continuar....");
 			}
 			getch();
 		}
 		
 		if (op == 3) {
-			gotoxy(2,2); printf("Informe o Nome do Aluno: ");
-			fflush(stdin);
-			fgets(nome,50,stdin);
+			if (t == NULL) {
+				printf("\n\n\n\n\t\tNenhum aluno cadastrado!\n\n\n\n\n\t Pressione uma tecla para continuar...");
+			} else {
+				gotoxy(2,2); printf("Informe o Nome do Aluno: ");
+				fflush(stdin);
+				fgets(nome,50,stdin);
+				removerQuebraLinha(nome);
 			
-			remover(&t, nome);
-			
+				remover(&t, nome, &achou);
+				
+				if (achou == 1) {
+					gotoxy(2,2); printf("\n\n Aluno encontrado e removido!\n\n Digite algo para continuar....");
+					achou = 0;
+				} else {
+					gotoxy(2,2); printf("\n\n Aluno não encontrado!\n\n Digite algo para continuar....");
+				}
+			}
 			getch();
 		}
-		
+
 		if (op == 4) {
 			if (t == NULL) {
-				printf("Arvore Vazia!");
+				printf("\n\n\n\n\t\tNenhum aluno cadastrado!\n\n\n\n\n\t Pressione uma tecla para continuar...");
 			} else {
+				printf("\n\tRepresentação por Parênteses Aninhados:\n\n\n");
 				parentesesAninhados(t);
+				printf("\n\n\n\n\n Pressione algo para continuar....");
 			}
 			getch();
 		}
@@ -102,11 +136,11 @@ void inserir(turma *aluno, char nome[50], float media, int ano) {
 		puts("Raiz");
 	} else {
 		if (stricmp(nome,(*aluno)->nomeCompleto) < 0) {
-			inserir(&(*aluno)->esquerda, nome, media, ano);
 			puts("Esquerda");
+			inserir(&(*aluno)->esquerda, nome, media, ano);
 		} else {
-			inserir(&(*aluno)->direita, nome, media, ano);
 			puts("Direita");
+			inserir(&(*aluno)->direita, nome, media, ano);
 		}
 	}
 }
@@ -115,15 +149,15 @@ void listarEmOrdem(turma aluno) {
 	if (aluno != NULL) {
 		listarEmOrdem(aluno->esquerda);
 		
-		printf("Nome....: %s \n", aluno->nomeCompleto);
-		printf("Media...: %.1f \n", aluno->mediaFinal);
-		printf("Ano.....: %i \n", aluno->anoCursou);
+		printf("Nome : %s ", aluno->nomeCompleto);
+		printf("\tMedia :%4.1f ", aluno->mediaFinal);
+		printf("\tAno : %i \n", aluno->anoCursou);
 		
 		listarEmOrdem(aluno->direita);
 	}
 }
 
-int remover(turma *aluno, char nome[50]) {
+int remover(turma *aluno, char nome[50], int *achou) {
 	turma t;
 	if (*aluno==NULL) {
 		return (1);
@@ -144,11 +178,11 @@ int remover(turma *aluno, char nome[50]) {
 			(*aluno)->anoCursou = t->anoCursou;
 		}
 		free(t);
-		gotoxy(2,2); printf("\nAluno encontrado e removido");
+		*achou = 1;
 	} else if (stricmp(nome,(*aluno)->nomeCompleto) < 0) {
-		remover(&((*aluno)->esquerda), nome);
+		remover(&((*aluno)->esquerda), nome, achou);
 	} else {
-		remover(&((*aluno)->direita), nome);
+		remover(&((*aluno)->direita), nome, achou);
 	}
 }
 
@@ -165,11 +199,19 @@ turma maior(turma *aluno) {
 
 void parentesesAninhados(turma aluno) {
 	if (aluno != NULL) {
-		printf(" ( ");
+		printf(" (");
 		printf("%s", aluno->nomeCompleto);
 		parentesesAninhados(aluno->esquerda);
 		parentesesAninhados(aluno->direita);
-		printf(" ) ");
+		printf(") ");
+	}
+}
+
+void removerQuebraLinha(char *nome) {
+	int len = strlen(nome);
+	if ((len > 0) && (nome[len - 1] == '\n')) {
+		// Remove quebra de linha.
+    	nome[--len] = 0;
 	}
 }
 
